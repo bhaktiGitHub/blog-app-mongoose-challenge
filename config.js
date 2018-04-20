ca18ad1196dcadcc7d5191c3b402dff6e6ff3191
Bhakti Wakchaure <bhakti.wakchaure@gmail.com>
2018-04-19 21:13:36 -0700

    added test file

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://bhakti:1234@ds257848.mlab.com:57848/thinkfuldb';
 exports.PORT = process.env.PORT || 8080;
